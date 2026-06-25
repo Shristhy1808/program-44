8a3747d9ed645ced8fbc17773af2c3b73cc5f59b
Shristhy1808 <sehgalshristhy@gmail.com>
2026-06-25 17:15:51 +0530

    Add files via upload

---
+++ main (22).c
@@ -0,0 +1,38 @@
+
+   #include <stdio.h>
+
+int main() {
+    int arr1[50], arr2[50], merged[100];
+    int n1, n2, i, j;
+
+    printf("Enter number of elements in first array (max 50): ");
+    scanf("%d", &n1);
+
+    printf("Enter %d elements for first array:\n", n1);
+    for (i = 0; i < n1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter number of elements in second array (max 50): ");
+    scanf("%d", &n2);
+
+    printf("Enter %d elements for second array:\n", n2);
+    for (i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    // Merge arrays
+    for (i = 0; i < n1; i++) {
+        merged[i] = arr1[i];
+    }
+    for (j = 0; j < n2; j++) {
+        merged[n1 + j] = arr2[j];
+    }
+
+    printf("Merged array:\n");
+    for (i = 0; i < n1 + n2; i++) {
+        printf("%d ", merged[i]);
+    }
+
+    return 0;
+}
